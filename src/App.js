dd3d30640a895c42ec4a8787cf0a5a7fa8ec7a75
Neel <it.neel97@yahoo.com>
2020-07-13 17:04:23 -0400

    125 creating layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <p>This is BurgerBuilder App Test!!</p>
+    <div >
+      <Layout>
+          <p>Test</p>
+      </Layout>
     </div>
   );
 }
