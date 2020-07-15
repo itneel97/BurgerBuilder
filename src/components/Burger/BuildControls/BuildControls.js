7ef2a2173423d8bf6bc5064e5d1edad37ddf8389
Neel <it.neel97@yahoo.com>
2020-07-15 19:38:16 -0400

    134 removing items from the burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,23 +3,24 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BulidControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'},
-    
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl 
-            key={ctrl.label} 
-            label={ctrl.label}
-            added={() => props.ingredientAdded(ctrl.type)}
-             />
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemove(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}  />
         ))}
-        
+
     </div>
 );
 
