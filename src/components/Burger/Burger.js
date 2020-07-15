0c6cfc93d0c35326a6e97b7e4bcbb64cf75ef0c2
Neel <it.neel97@yahoo.com>
2020-07-15 18:16:35 -0400

    130 reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -14,12 +14,27 @@ const burger = (props) => {
      const temp5 = Object.keys([...Array(props.ingredients["meat"])]) // this returns how many elements are there with same key like there are two meat in the ingredients
      */
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return (<BurgerIngredient key={igKey + i} type={igKey} />);
             });
-        });
+        })// reduce accepts function as a input and here it has two arguments previousValue(arr) and currentValue(el) 
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []); // [] is initial value
+    /** 
+     * we can use flatMap as well
+        var arr = [1, 2, 3, 4];
+        arr.flatMap(x => [x, x * 2]);
+        // is equivalent to
+        arr.reduce((acc, x) => acc.concat([x, x * 2]), []);
+        // O/P: [1, 2, 2, 4, 3, 6, 4, 8]
+    */
+
+    if (transformedIngredients === 0) {
+        transformedIngredients = <p>Please start adding Ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger} >
