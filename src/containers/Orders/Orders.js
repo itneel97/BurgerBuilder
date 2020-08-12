74056c78cb1434a1bcb718ef68d5dbb054172b66
Neel <it.neel97@yahoo.com>
2020-08-12 19:45:56 -0400

    230 fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler'
+class Orders extends Component {
 
-class Orders extends Component{
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                console.log(res.data) // res.data holds data we get from firebase
+                const fetchOrders = []
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                this.setState({ loading: false, orders: fetchOrders })
+            }).catch(error => {
+                this.setState({ loading: false })
+            })
+    }
+
     render() {
 
-        return(
+        return (
             <div>
                 <Order />
                 <Order />
@@ -13,4 +37,4 @@ class Orders extends Component{
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios) ;
